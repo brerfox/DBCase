7b46756510e879ca133fff7e0df0dbf1f1fa1006
kinmanz <luga2012@yandex.ru>
2017-08-12 15:51:44 +0300

    Thread safe PropertyService

--- Backend/src/main/webapp/js/jquery-example.js
+++ Backend/src/main/webapp/js/jquery-example.js
@@ -1,8 +1,13 @@
 
 $(document).ready( function() {
 
-
-
+    $(document).on('click', '#logout', function() {
+        console.log(window.location.host + '/api/logout');
+        $.get("http://" + window.location.host + '/api/logout', {}, function(data){
+            console.log(window.location.host + '/api/logout');
+            window.location.replace("/");
+        });
+    });
     $(document).on('click', '#login-form-button', function() {
 
         var login = $('#login-form-login').val();
@@ -18,16 +23,18 @@ $(document).ready( function() {
     });
 
     function afterAuth() {
-        $.get(window.location.pathname + 'api/get/deal', {page_id: 1, page_size: 100}, function(data){
-            console.log(data);
-            data.forEach(function(element) {
-                $('#data_box').append('<li class="list-group-item">Deal ID: ' + element.deal_id + ', Deal amount: ' + element.deal_amount + ', </li>')
-            })
-        });
+        // $.get(window.location.pathname + 'api/get/deal', {page_id: 1, page_size: 100}, function(data){
+        //     // console.log(data);
+        //     // data.forEach(function(element) {
+        //     //     $('#data_box').append('<li class="list-group-item">Deal ID: ' + element.deal_id + ', Deal amount: ' + element.deal_amount + ', </li>')
+        //     // })
+        //
+        // });
+        window.location.replace("/overview.html");
     }
 
     function dbCheck () {
-        $.get(window.location.pathname + 'api/db_is_alive', {}, function(data){
+        $.get('http://' + window.location.host + '/api/db_is_alive', {}, function(data){
             console.log(data);
             if(data == true) {
                 $('#db_info').removeClass("alert-warning");
